d3fe2a14219514e010ca8afbb00aef8211e26673
Václav Chlumský <chlumskyvaclav@gmail.com>
2023-01-23 10:43:52 +0100

    clear estimates for former topjobs

--- src/scheduler/fifo.c
+++ src/scheduler/fifo.c
@@ -1070,6 +1070,16 @@ main_sched_loop(status *policy, int sd, server_info *sinfo, schd_error **rerr)
 						njob->name, "Error in add_job_to_calendar");
 				}
 				/* else cal_rc == 0: failed to add to calendar - continue on */
+			} else {
+				if (njob->job->est_start_time != -1 || njob->job->est_execvnode != 0x0) {
+					struct attrl attr = {0};
+					attr.name = ATTR_estimated;
+					attr.resource = "exec_vnode";
+					attr.value = "";
+
+					update_job_attr(sd, njob, ATTR_estimated, "start_time",
+						"", &attr, UPDATE_NOW);
+				}
 			}
 
 			/* Need to set preemption status so that soft limits can be checked
